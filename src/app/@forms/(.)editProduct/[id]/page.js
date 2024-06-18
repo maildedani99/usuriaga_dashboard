ea29c61381f48ae5c09f4eba798b6fe544c07215
dani <maildedani9@gmail.com>
2024-06-18 22:27:03 +0200

    wip

--- src/app/@forms/(.)editProduct/[id]/page.js
+++ src/app/@forms/(.)editProduct/[id]/page.js
@@ -1,28 +1,16 @@
-"use client"
-import { useEffect, useState } from "react";
 import EditProductForm from "../../../components/EditProductForm";
 import Modal from "../../../components/Modal";
 import { getProductById, getSubcategories } from "../../../lib/data";
 
 
-export  async function EditProduct({ params }) {
+export default async function EditProduct({ params }) {
 
-    const [product, setProduct] = useState({});
-
-    const onGetProductByID = async () => {
-        const product = await getProductById(params.id)
-        console.log(product)
-        setProduct(product)
-    }
-
-    useEffect(()=> {
-        onGetProductByID()
-    },[params])
+    const product = await getProductById(params.id)
 
 
     return (
         <Modal>
-            <EditProductForm product={product ? product : {}}  />
+            <EditProductForm product={product[0]}  />
         </Modal>
     )
 }
